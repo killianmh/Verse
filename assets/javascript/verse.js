a93fb2d2941b841d7123da689fb0a1d6e839a736
stevenhorkey <stevenhorkey@gmail.com>
2018-03-12 19:32:52 -0700

    added onclick to get user line and made title page scroll to section two

--- assets/javascript/verse.js
+++ assets/javascript/verse.js
@@ -1,24 +1,45 @@
 
 game = {
     variables : {
-        username : ""
+        username : "",
+        userLine : "",
     },
     functions : {
 
     },
     onClicks : {
-        userSubmit : function(){
+        getUserName : function(){
+            // This function gets the name that the user inputs on the title page and saves it as the username to be used throughout the game. 
             $('#user-name-submit').on('click', function(){
                 var input = $('#user-name').val().trim();
                 if (input !== ""){
                     game.variables.username = input;
                     console.log(game.variables.username)
+
+                    // Scrolls page to next section
+                    $('html, body').animate({
+                        scrollTop: $(".info-character").offset().top
+                    }, 400);   
+                }
+                             
+            });
+        },
+        getUserLine : function(){
+            // This function eventually needs to be a timeout instead of an onclick. It grabs the input that the user filled out and uses it to make their final rap image. If they don't enter anything, they fail. 
+            $('#user-line-submit').on('click',function(){
+                var input = $('#user-line').val().trim();
+                if (input !== ""){
+                    game.variables.userLine = input;
+                    console.log(game.variables.userLine)
+                } else {
+                    alert("You failed bro.")
                 }
             });
-        }
+        },
     }
 }
 
 $(document).ready(function(){
-    game.onClicks.userSubmit();
+    game.onClicks.getUserName();
+    game.onClicks.getUserLine();
 });
