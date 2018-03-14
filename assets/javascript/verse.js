059a32e15eda5264db778362042921a5a269d48f
stevenhorkey <stevenhorkey@gmail.com>
2018-03-13 21:36:07 -0700

    end of 3/13/18

--- assets/javascript/verse.js
+++ assets/javascript/verse.js
@@ -1,11 +1,54 @@
-
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBx9Yt8xafU7Zz3zuAczc1RfoW-EktImyI",
+authDomain: "verse-7f3f4.firebaseapp.com",
+databaseURL: "https://verse-7f3f4.firebaseio.com",
+projectId: "verse-7f3f4",
+storageBucket: "",
+messagingSenderId: "374838587919"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+  
 game = {
     variables : {
         username : "",
         userLine : "",
+        hypeChoice : "",
     },
     functions : {
-
+        generateGame : function(){
+            database.ref().set({
+                players : {
+                    one : {
+                        username : "",
+                        userLine : "",
+                        userChar : "",
+                        
+                    },
+                    two : {
+                        username : "",
+                        userLine : "",
+                        userChar : "",
+                    }
+                }
+            });
+        },
+        checkPlayers : function(){
+            
+            database.ref().once("value", function(snapshot){
+                console.log(snapshot.val().players.one.username);
+                if (snapshot.val().players.one.username === ""){
+                    database.ref('players/one').update({
+                        username : game.variables.username
+                    })
+                } else if (snapshot.val().players.two.username === "") {
+                    database.ref('players/two').update({
+                        username : game.variables.username
+                    })
+                }
+            });
+        },
     },
     onClicks : {
         getUserName : function(){
@@ -20,8 +63,30 @@ game = {
                     $('html, body').animate({
                         scrollTop: $(".info-character").offset().top
                     }, 400);   
-                }
+                    game.functions.checkPlayers()
+                };
                              
+            });
+            $(document).bind('keypress', function(event) {
+                if(event.keyCode==13){
+                     $('#user-name-submit').trigger('click');
+                 }
+            });
+            
+        },
+        chooseChar : function(){
+            $('.user-chars').on('click',function(){
+                var choice = $(this);
+                console.log(choice.attr('id'));
+            });
+        },
+        chooseHype : function(){
+            $('.hype-char-img').on('click',function(){
+                var choice = $(this).data('name')
+                game.variables.hypeChoice = choice;
+                var hypeMan = $(this).clone();
+                hypeMan.addClass('hype-choice-api');
+                $('.user-rap').append(hypeMan);
             });
         },
         getUserLine : function(){
@@ -40,6 +105,10 @@ game = {
 }
 
 $(document).ready(function(){
+    game.functions.generateGame();
     game.onClicks.getUserName();
+    game.onClicks.chooseChar();
+    game.onClicks.chooseHype();    
     game.onClicks.getUserLine();
+
 });
